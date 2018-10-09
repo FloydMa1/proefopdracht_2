c1a3f56a422b0ee4e0325f468fa15f0e9f59a896
FloydMa1 <f.lehman@ziggo.nl>
2018-10-09 09:18:10 +0200

    Dialog game

---
+++ DialogSystem/Assets/scripts/TextLines.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextLines : MonoBehaviour {
+
+    string[] dialogs = new string[5] 
+    {
+        "Hey buddy!, How are you?",
+        "Can i chear you up?",
+        "*Giving V-Bucks*",
+        "Too bad",
+        "FLIP YOU!!"
+    };
+
+    string[] anwsers = new string[1]
+    {
+        "Give me V-Bucks"
+    };
+
+    public Dictionary<string, string> dialogTexts;
+
+    public void Start()
+    {
+        dialogTexts = new Dictionary<string, string>();
+        dialogTexts.Add("start", dialogs[0]);
+
+        dialogTexts.Add("Not so happy", dialogs[1]);
+        dialogTexts.Add("Give V-Bucks", dialogs[2]);
+        dialogTexts.Add("anwser", anwsers[0]);
+
+        dialogTexts.Add("i'm Good", dialogs[3]);
+
+        dialogTexts.Add("leave", dialogs[4]);
+    }
+
+}
---
+++ DialogSystem/Assets/scripts/TreeAlgorithem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TreeAlgorithem : MonoBehaviour {
+
+    public GameObject[] buttons;
+
+    public GameObject replay;
+
+    public Text anwserText;
+    public Text dialogText;
+    TextLines textLines;
+
+    public void Start()
+    {
+        textLines = GetComponent<TextLines>();
+        dialogText.text = textLines.dialogTexts["start"];
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Good()
+    {
+        dialogText.text = textLines.dialogTexts["i'm Good"];
+    }
+
+    public void Bad()
+    {
+        buttons[0].active = false;
+        buttons[1].active = false;
+        buttons[2].active = true;
+        dialogText.text = textLines.dialogTexts["Not so happy"];
+        anwserText.text = textLines.dialogTexts["anwser"];
+    }
+
+    public void GiveBucks()
+    {
+        
+        dialogText.text = textLines.dialogTexts["Give V-Bucks"];
+    }
+
+    public void Leave()
+    {
+        dialogText.text = textLines.dialogTexts["leave"];
+    }
+
+}
